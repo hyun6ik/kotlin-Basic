7977bbec5a2fff78ca992efcfae3d6805646fd3d
hyun6ik <fkanwm@naver.com>
2022-08-29 22:45:23 +0900

    feat : 동반 객체(Companion Object)

---
+++ src/main/kotlin/Lec12/CompanionObject.kt
@@ -0,0 +1,15 @@
+package Lec12
+
+class MyClass private constructor() {
+
+    companion object {
+        val a = 1234
+
+        fun newInstance() = MyClass()
+    }
+}
+
+fun main() {
+    println(MyClass.a)
+    println(MyClass.newInstance())
+}
