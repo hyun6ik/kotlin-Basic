94ae88be765fc06063a270b5f54384e560a44b40
hyun6ik <fkanwm@naver.com>
2022-09-05 01:25:00 +0900

    feat : 비동기-논블로킹 프로그래밍

---
+++ src/main/kotlin/Lec26/Thread.kt
@@ -0,0 +1,11 @@
+package Lec26
+
+fun main() {
+    for (i in 0..5) {
+        val thread = Thread(Runnable {
+            println("current-thread-name : ${Thread.currentThread().name}")
+        })
+        thread.start()
+    }
+    println("current-thread-name : ${Thread.currentThread().name}")
+}
